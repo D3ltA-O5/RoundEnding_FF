b68d2dec657aaf37c374532cd4c29c01cca45c1d
D3ltA_O5 <cybercodeveloper@gmail.com>
2024-09-05 09:44:04 +0300

    First plugin ver

---
+++ RoundEnding_FF/Config.cs
@@ -0,0 +1,26 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace RoundEndingFF
+{
+    public class Config : IConfig
+    {
+        [Description("Включен ли плагин.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Сообщение, которое отображается всем игрокам, когда включен friendly fire.")]
+        public string BroadcastMessage { get; set; } = "Friendly fire is now enabled! Have fun for the last seconds!";
+
+        [Description("Длительность показа сообщения (в секундах).")]
+        public ushort BroadcastDuration { get; set; } = 10;
+
+        [Description("Задержка окончания раунда перед перезапуском (в секундах).")]
+        public float RoundEndDelay { get; set; } = 10f;
+
+        [Description("Включить режим отладки.")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Цвет текста в broadcast-сообщении.")]
+        public string BroadcastColor { get; set; } = "red";
+    }
+}
---
+++ RoundEnding_FF/Plugin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using System.Collections.Generic;
+using Exiled.Events.EventArgs.Server;
+
+namespace RoundEndingFF
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "RoundEnding_FF";
+        public override string Author => "D3ltA_O5";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(8, 11, 0);
+
+        private CoroutineHandle _roundEndHandle;
+
+        public override void OnEnabled()
+        {
+            if (!Config.IsEnabled)
+                return;
+
+            Exiled.Events.Handlers.Server.RoundEnded += OnRoundEnded;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundEnded -= OnRoundEnded;
+            base.OnDisabled();
+        }
+
+        private void OnRoundEnded(RoundEndedEventArgs ev)
+        {
+            // Активируем friendly fire
+            Server.FriendlyFire = true;
+
+            // Формируем сообщение с цветом
+            string coloredMessage = $"<color={Config.BroadcastColor}>{Config.BroadcastMessage}</color>";
+
+            // Показываем сообщение всем игрокам
+            Map.Broadcast(Config.BroadcastDuration, coloredMessage);
+
+            // Задерживаем конец раунда
+            _roundEndHandle = Timing.CallDelayed(Config.RoundEndDelay, () =>
+            {
+                // Деактивируем friendly fire
+                Server.FriendlyFire = false;
+                Server.Restart();
+            });
+        }
+    }
+}
